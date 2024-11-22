27b51f9457d090a03cf9742edac85fd222313240
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2024-11-22 17:15:20 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um Número:"))
+
+if numero > 0:
+    print (f"O número {numero} é positivo!")
+elif numero < 0: 
+    print(f"O número {numero} é negativo!")
+else:
+    print(f"O número {numero} é 0!")        
+
