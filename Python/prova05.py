9fe81de82bb8e433bd6cd6bfc907015bac0912b3
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-02-11 23:49:27 -0300

    Add files via upload

---
+++ Python/prova05.py
@@ -0,0 +1,13 @@
+numero1 = float(input("Informe o primeiro número: "))
+numero2 = float(input("Informe o segundo número: "))
+numero3 = float(input("Informe o terceiro número: "))
+
+def maior_numero (num1, num2, num3):
+    if(num1 > num2 and num1 > num3):
+        return f"O maior numéro é o primeiro com o valor {num1}"
+    elif num2 > num1 and num2 > num3:
+        return f"O maior numéro é o segundo com o valor {num2}"
+    else:
+        return f"O maior numéro é o terceiro com o valor {num3}"
+    
+print(maior_numero(numero1, numero2, numero3))  
