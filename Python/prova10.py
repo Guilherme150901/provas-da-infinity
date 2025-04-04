c2c05536a0c9b1eedb8d37f9ab13571b2c0aa228
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:45:01 -0300

    Update prova10.py

--- Python/prova10.py
+++ Python/prova10.py
@@ -1,3 +1,9 @@
+#[PYIA-A10] Desenvolva uma aplicação utilizando o framework Flet que permita ao usuário preencher
+# um formulário de contato. O formulário deve incluir três campos: um campo de texto para o nome, 
+# um campo de texto para o email e um campo de texto para a mensagem. Após o usuário preencher esses campos,
+# deve haver um botão de envio. Quando o usuário clicar no botão de envio, os dados devem ser processados e
+# uma mensagem de confirmação deve ser exibida na tela, indicando que o formulário foi enviado com sucesso.
+
 import flet as ft
 
 def enviarContato(e):
