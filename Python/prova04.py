487722cd2818d359453195488d3f529efee99572
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:53:28 -0300

    Update prova04.py

--- Python/prova04.py
+++ Python/prova04.py
@@ -1,3 +1,8 @@
+#[PYIA-A04] Crie uma função chamada media que receberá três números como argumentos. 
+# Esta função deve calcular a média aritmética desses três números. Para fazer isso, 
+# some os três números e, em seguida, divida o resultado por três. Por fim, a função
+# deve retornar o valor da média aritmética calculada.
+
 numero1 = float(input("Digite o primeiro número: "))
 numero2 = float(input("Digite o segundo número: "))
 numero3 = float(input("Digite o terceiro número: "))
