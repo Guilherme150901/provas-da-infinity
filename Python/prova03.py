0e9cab7cb6abc79b7a4ad2f14fb1504b0e9ed9b2
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:54:57 -0300

    Update prova03.py

--- Python/prova03.py
+++ Python/prova03.py
@@ -1,3 +1,10 @@
+#[PYIA-A03] Crie um dicionário para armazenar o nome e o preço de cinco produtos.
+# Peça ao usuário para inserir o nome de cada produto e o respectivo preço. À medida
+# que o usuário fornece os dados, armazene cada produto como uma chave no dicionário e
+# o preço como o valor associado a essa chave. Após a inserção de todos os produtos e preços,
+# calcule o valor total da compra somando todos os preços armazenados no dicionário. Por fim,
+# exiba o valor total da compra.
+
 produtos = {}
 soma = 0
 
