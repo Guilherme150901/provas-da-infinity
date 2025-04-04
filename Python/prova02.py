5d3b49a40af3f330a59793c86b8939842c7bb5ea
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:56:25 -0300

    Update prova02.py

--- Python/prova02.py
+++ Python/prova02.py
@@ -1,3 +1,9 @@
+#[PYIA-A02] Crie um dicionário que irá armazenar informações de um contato, incluindo o nome,
+# o telefone e o email. Peça ao usuário para fornecer esses dados, solicitando que ele insira
+# o nome do contato, o número de telefone e o endereço de email. Após coletar todas as informações
+#necessárias, exiba o conteúdo do dicionário, mostrando todas as informações do contato inserido
+# pelo usuário.
+
 contato = {
     "Nome do contado": "",
     "Número de telefone": "",
@@ -12,4 +18,3 @@ contato["Número de telefone"] = numero_de_telefone
 contato["Email"] = email
 
 print(contato)
-
