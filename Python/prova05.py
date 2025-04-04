9217b216545d1afe61db86ad8a73fd5ec8d5227f
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:52:15 -0300

    Update prova05.py

--- Python/prova05.py
+++ Python/prova05.py
@@ -1,3 +1,8 @@
+#[PYIA-A05] Crie uma função chamada maior_numero que receberá três números como argumentos.
+# Esta função deve comparar os três números e identificar qual deles é o maior. Para isso,
+# utilize uma estrutura de controle que verifique qual número é maior que os outros dois. 
+# A função deve então retornar o maior número encontrado.
+
 numero1 = float(input("Informe o primeiro número: "))
 numero2 = float(input("Informe o segundo número: "))
 numero3 = float(input("Informe o terceiro número: "))
