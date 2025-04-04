0a2761ce66f57480cc4f5e71a65e71e3ec66b295
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-04-03 21:47:13 -0300

    Update prova09.py

--- Python/prova09.py
+++ Python/prova09.py
@@ -1,11 +1,15 @@
+#[PYIA-A09] Desenvolva uma aplicação utilizando o framework Flet que permita ao usuário adicionar itens a
+# uma lista de tarefas. A interface da aplicação deve incluir um campo de entrada de texto para o usuário digitar
+# o nome da tarefa e um botão para adicionar a tarefa à lista. Quando o usuário clicar no botão, o item deve ser
+# adicionado a uma lista exibida na tela, mostrando todas as tarefas que foram incluídas até o momento. A lista de
+# tarefas deve ser atualizada dinamicamente sempre que um novo item for adicionado.
+
 import flet as ft
 
 def main(page: ft.Page):
     page.title = "Gerenciador de Tarefas"
 
     tarefas = []
-
-    def adicionar_tarefa(e):
         tarefa = campo_texto.value.strip()  
         if tarefa:  
             tarefas.append(tarefa) 
@@ -14,6 +18,8 @@ def main(page: ft.Page):
 
     def atualizar_tarefas():
         lista_tarefas.controls.clear() 
+
+    def adicionar_tarefa(e):
         for tarefa in tarefas:
             lista_tarefas.controls.append(ft.Text(tarefa)) 
         page.update()  
