997cc653e2d6dcae933ad8186d4384726b3d8153
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-05-08 23:00:30 -0300

    Add files via upload

---
+++ Python/prova15.sql
@@ -0,0 +1,12 @@
+-- [PYIA-A15] Crie uma tabela chamada Clientes com as colunas ID, Nome,Idade
+-- e Cidade. Defina a coluna ID como a chave primária e autoincrementável.
+
+CREATE DATABASE IF NOT EXISTS MEUBANCO;
+USE meubanco;
+
+CREATE TABLE IF NOT EXISTS clientes(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    idade INT NOT NULL,
+    cidade VARCHAR(50) NOT NULL
+);
