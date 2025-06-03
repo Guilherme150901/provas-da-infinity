6d8cfba1bbf5d6292803b8e37c16948eda34b4dd
Guilherme Andrade <146844935+Guilherme150901@users.noreply.github.com>
2025-06-02 23:38:29 -0300

    Update classes.py

--- Python/Prova18 - Projeto/classes.py
+++ Python/Prova18 - Projeto/classes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Produto:
     def __init__(self, id_produto, nome, descricao, quantidade, preco):
         self.id_produto = id_produto
@@ -14,3 +16,13 @@ class Produto:
         if self.quantidade < 0:
             self.quantidade = 0
             return f"Estoque insuficiente para {self.nome}. Quantidade ajustada para 0."
+
+class Venda:
+    def __init__(self, id_venda, id_produto, quantidade_vendida, data_venda):
+        self.id_venda = id_venda
+        self.id_produto = id_produto
+        self.quantidade_vendida = quantidade_vendida
+        self.data_venda = data_venda
+
+    def __str__(self):
+        return f"Venda {self.id_venda} | Produto ID {self.id_produto} | Quantidade: {self.quantidade_vendida} | Data: {self.data_venda}"
